b766b0dad732062746a1f49ca5e5b5096afee1ef
KetikovAlexander <spok800@yandex.ru>
2022-12-05 22:41:33 +0300

    Task5

---
+++ Seminar1Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача №5. На вход принимает число (N), а на выходе показывает все целые числа от -N до N.
+// N=2 -> -2,-1,0,1,2
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for(int i = -N; i <= N; i++)
+{
+Console.Write(i  + " ");
+}

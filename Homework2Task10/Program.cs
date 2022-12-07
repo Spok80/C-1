e7046e4ae3c66ca7823fd7e41c4a73000de884e8
KetikovAlexander <spok800@yandex.ru>
2022-12-07 23:42:02 +0300

    Task 10 final

---
+++ Homework2Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10. Принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number < 100 || number >= 1000) 
+{
+Console.WriteLine($"Число {number} не трехзначное, Введите число");
+number = Convert.ToInt32(Console.ReadLine());
+}
+int digit2 = number / 10 % 10;
+Console.WriteLine($"Цифра {digit2} - вторая цифра числа {number}");

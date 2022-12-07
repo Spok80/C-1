38172e05511274310098432a1b4a47e543b58f3b
KetikovAlexander <spok800@yandex.ru>
2022-12-07 22:23:49 +0300

    Task11 final

---
+++ Seminar2Task11/Program.cs
@@ -0,0 +1,9 @@
+﻿// Ввод случайного троичного числа и удаление второй цифры
+
+int number = new Random().Next(100, 1000); // интервал от 100 до 999
+int digit3 = number % 10;
+int digit2 = number % 100 / 10;
+int digit1 = number / 100;
+
+Console.WriteLine($"Случайное число = {number}, без второй цифры = {digit1}{digit3}");
+// Console.WriteLine(number/100*10+number%10); // Сразу поиск первой и 3 цифры числа

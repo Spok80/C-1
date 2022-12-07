d6102c8c2a56e79c65779a20151e73b433f06155
KetikovAlexander <spok800@yandex.ru>
2022-12-07 23:09:22 +0300

    Task14 final

---
+++ Seminar2Task14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 14. На вход принимает число и проверяет, кратно ли оно одновременно и 7 и 23
+// 7 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ostatok1 = number % 7;
+int ostatok2 = number % 23;
+
+if (ostatok1 == 0 && ostatok2 == 0)
+{
+Console.WriteLine($"Число {number} кратно и 7 и 23");
+}
+else
+{
+Console.WriteLine($"Число {number} не кратно и 7 и 23");   
+}

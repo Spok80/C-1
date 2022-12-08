75a117b181a53b6ae724ef1abdf35f13120b4535
KetikovAlexander <spok800@yandex.ru>
2022-12-08 22:11:52 +0300

    Task 13 final

---
+++ Homework2Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13. Программа выводит 3 цифру заданного числа или сообщает, что ее нет.
+
+Console.WriteLine("Введите число");
+double number = Convert.ToInt32(Console.ReadLine());
+double ostatok = number / 10;
+if (number < 99)
+{
+Console.WriteLine("Третьей цифры нет");   
+}
+else
+{
+while (ostatok >= 1)
+{
+ ostatok = ostatok / 10; 
+}  
+double digit3 = (int)((ostatok * 1000) % 10);
+Console.WriteLine($"Третья цифра - {digit3}");
+}

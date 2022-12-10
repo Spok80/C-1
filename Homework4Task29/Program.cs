d3a29baebcb946307f5b6d036b803bffb0635d49
KetikovAlexander <spok800@yandex.ru>
2022-12-10 20:24:53 +0300

    Task29 final

---
+++ Homework4Task29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 29. Задать массив из 8 элементов и вывести его на экран. Данные вводятся пользователем.
+
+int [] array = new int [8]; // Массив array из 8 элементов целых чисел
+int number = 0;
+for (int i = 0; i < 8; i++)
+{
+  Console.WriteLine("Введите число");
+  number = Convert.ToInt32(Console.ReadLine());
+  array[i] = number;
+}
+Console.WriteLine(string.Join("," , array));

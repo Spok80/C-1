fda732b558a38430130b5119a5be21db30d0cb2b
KetikovAlexander <spok800@yandex.ru>
2022-12-10 19:32:22 +0300

    Task27 final

---
+++ Homework4Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27. Принимает число и выводит сумму цифр в числе
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = number;
+int i = 1;
+int result = 0;
+int digite = 0;
+for (i=1; number != 0; i++)
+{
+digite = number % 10;
+number = number / 10;
+result = result + digite;
+}
+Console.WriteLine($"Сумма цифр числа {number1} - {result}");

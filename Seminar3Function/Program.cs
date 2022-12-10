6b6371bbd3b878e64836d7ef68bb4f65ccdfe071
KetikovAlexander <spok800@yandex.ru>
2022-12-10 15:43:39 +0300

    Function

---
+++ Seminar3Function/Program.cs
@@ -0,0 +1,13 @@
+﻿// Нахожжение корня числа и возведения числа в степень
+
+int a = 3;
+int b = 3;
+
+double resSqrt = Math.Sqrt(a); // Вычисление корня числа a
+double resPow = Math.Pow(a,b); // Возведение в степень b числа a
+
+resSqrt = (int)(resSqrt); // Выводит целую часть числа БЕЗ ОКРУГЛЕНИЯ
+
+Console.WriteLine($"{resSqrt:f2}"); // Вывод числа с 2 знаками после запятой
+Console.WriteLine($"{resPow}");
+

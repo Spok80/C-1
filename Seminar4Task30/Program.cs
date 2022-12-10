5b7022a466b043b21a0212293d673a1e788689c2
KetikovAlexander <spok800@yandex.ru>
2022-12-10 20:05:59 +0300

    Task30 final

---
+++ Seminar4Task30/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 30. Заполнить массив из 8 элементов нулями и единицами. Вывести на экран.
+
+int [] array = new int[8]; // Массив array из 8 элементов целых чисел
+for (int i = 0; i < 8; i++)
+{
+   array[i] = new Random().Next(0,2);
+}
+Console.WriteLine(string.Join("," , array));

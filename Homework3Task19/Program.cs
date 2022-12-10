d3018b11055e9e027a00cc4bf0dbcab487870012
KetikovAlexander <spok800@yandex.ru>
2022-12-10 18:44:20 +0300

    Task19 final

---
+++ Homework3Task19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 19. Принимает на вход пятизначное число и проверяет, является ли оно полиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number < 10000 || number > 99999) 
+{
+Console.WriteLine($"Число {number} не является пятизначным, Введите пятизначное число");
+number = Convert.ToInt32(Console.ReadLine());
+}
+int digite1 = number / 10000;
+int digite2 = number / 1000 % 10;
+int digite4 = number / 10 % 10;
+int digite5 = number % 10;
+
+if (digite1 == digite5 && digite2 == digite4)
+{
+Console.WriteLine($"Число {number} - является полиндромом");
+}
+else
+{
+Console.WriteLine($"Число {number} - не является полиндромом");   
+}

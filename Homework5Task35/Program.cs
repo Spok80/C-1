fdc7366d9a5af6633967b599053fc557fb44e4b9
KetikovAlexander <spok800@yandex.ru>
2022-12-15 22:23:39 +0300

    Task 35 final

---
+++ Homework5Task35/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел в диапазоне [0, 150]. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+
+// 5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] GetRandomArray(int length, int leftRange, int rightRange)
+{
+    int[] array = new int[length]; 
+    Random rand = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+
+    return array;
+}
+
+int sum = 0;
+int[] FindSum(int[] array)
+{
+
+     for(int i = 0; i < array.Length; i++)
+     {
+        if (array[i] >= 10 && array[i] <= 99)
+        sum = sum + 1; // Если число из диапазона [10,99], учитываем его в общем количестве чисел
+     }
+        
+     return array;
+}
+
+const int LENGTH = 123;
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 150;
+
+int[] massive = GetRandomArray(LENGTH, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine($"{string.Join(", ", massive)}");
+FindSum(massive);
+Console.WriteLine($"Количество чисел в массиве в диапазоне [10,99] - {sum}");

842aee18c9536c76c1d183ab7b9eee618917db82
KetikovAlexander <spok800@yandex.ru>
2022-12-17 21:40:02 +0300

    finish

---
+++ Rekursiya/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу перевода десятичного числа в двоичное
+
+int number = 13;
+
+PrintBinaryView(number);
+
+void PrintBinaryView(int n)
+{
+    if (n<=0)
+    {
+        return;
+    }
+    PrintBinaryView(n / 2);
+    Console.Write(n % 2);
+}
+

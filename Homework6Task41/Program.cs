fec6d360d94331ecd56d964fc6300c84658c3611
KetikovAlexander <spok800@yandex.ru>
2022-12-17 23:14:54 +0300

    Task 41 final

---
+++ Homework6Task41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода слова "stop"
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+string number=InputNumber();
+
+int sum = 0;
+while (number != "STOP")
+{
+  int count = Convert.ToInt32(number);
+  if (count > 0) sum++;  
+  number=InputNumber();
+}
+Console.WriteLine($"Число введенных чисел больше нуля - {sum}");
+
+string InputNumber()
+{
+    Console.WriteLine("Введите число");
+    string number = Console.ReadLine();
+    return number;
+}

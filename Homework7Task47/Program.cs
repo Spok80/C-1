27f2fb1dd8b11bb86579b3b176661f4c5a174287
KetikovAlexander <spok800@yandex.ru>
2022-12-24 19:53:50 +0300

    Task47 final

---
+++ Homework7Task47/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// ​
+// m = 3, n = 4.
+// ​
+// 0,5 7 -2 -0,2
+// ​
+// 1 -3,3 8 -9,9
+// ​
+// 8 7,8 -7,1 9
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[,] GetRandomMatrix(int rows, int columns, double leftRange = -10.00, double rightRange = 10.00)
+{
+    double[,] matr = new double[rows, columns];
+
+    Random rand = new Random();
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Math.Round((rand.NextDouble() * (rightRange - leftRange) + leftRange), 1); // СОздание дробного числа и округление до 1 символа после запятой
+        }
+    }
+
+    return matr;
+}
+
+void PrintMatrix(double[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+    }
+}
+
+int rowsCount = ReadInt("Введите число строк");
+int columnsCount = ReadInt("Введите число столбцов");
+double[,] matrix = GetRandomMatrix(rowsCount, columnsCount);
+PrintMatrix(matrix);

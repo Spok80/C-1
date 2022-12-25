f41a2a31c8c120d0a7d61f02f592c42feb00da46
KetikovAlexander <spok800@yandex.ru>
2022-12-25 23:51:33 +0300

    Task 62 final

---
+++ Homework8Task62/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 62. Заполните спирально массив 4 на 4.
+
+// Например, на выходе получается вот такой массив:
+
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+int[,] FillMatrix()
+{
+    int[,] Matrix1 = new int[4,4];
+    int n = 1;
+    int i = 0;
+    int j = 0;
+    
+    while (n <= 16)
+    {
+
+        Matrix1[i, j] = n;
+        n = n + 1;
+        if (i <= j + 1 && i + j < 3)
+        {
+        j++;
+        }
+        else if (i < j && i + j >= 3)
+        {
+        i++;
+        }
+        else if (i >= j && i + j > 3)
+        {
+        j--;
+        }
+        else
+        i--;
+        
+    }
+    return Matrix1;
+}
+
+void PrintMatrix(int[,] Matrix)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < 4; j++)
+        {
+            {
+            Console.Write(Matrix[i, j] + " ");
+            }
+        }
+    }
+}
+
+int[,] Matrix = FillMatrix();
+PrintMatrix(Matrix);

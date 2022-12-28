0a9c31a57836180dc0f63c6057a18a15aea4febf
KetikovAlexander <spok800@yandex.ru>
2022-12-28 22:05:57 +0300

    Rekursiya final

---
+++ Seminar9Rekursiya/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = 3;
+
+int Factorial(int n)
+{
+    if (n < 0) return -1;
+    if (n == 0 || n == 1) return 1;
+    return n * Factorial(n - 1); 
+}
+Console.WriteLine(Factorial(number));

ca79d30f5c3bb060ed6be673e1f1fde587e38918
KetikovAlexander <spok800@yandex.ru>
2022-12-28 23:31:51 +0300

    Task 66 final

---
+++ Homework9Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int min = 1;
+int max = 15;
+int sum = 0;
+
+if (max < min || max < 0 || min < 0)
+{
+    Console.Write("Не корректно заданы значения M и N");
+    return;
+}
+int Summa(int m, int n)
+{
+    
+    if (n == m) return sum + m;
+    return sum + n + (Summa(m, n - 1));;
+}
+Console.Write(Summa(min, max));
+
